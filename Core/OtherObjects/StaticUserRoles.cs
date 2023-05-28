b8b91ce38a6ce7dd2600ccdefabc0234cd7b14c4
Taha991 <tahamohamedman@gmail.com>
2023-05-28 13:53:29 +0200

    Adding Dtos + Static-Role

---
+++ Core/OtherObjects/StaticUserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtAuthAspNet7.Core.OtherObjects
+{
+    public static class StaticUserRoles
+    {
+        public static string OWNER = "OWNER";
+        public static string ADMIN = "ADMIN";
+        public static string USER = "USER";
+
+
+    }
+}
